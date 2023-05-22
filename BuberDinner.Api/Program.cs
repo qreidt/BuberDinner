dbabd452c5f95e73479cb65d96bbe797231e7d14
Caio Reidt <caioreidt@gmail.com>
2023-05-22 18:57:31 -0300

    Add Error Endpoint and Custom Problem Details Factory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,17 +1,23 @@
+using BuberDinner.Api;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplicationLayer();
     builder.Services.AddInfrastructureLayer();
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
+
+    app.UseExceptionHandler("/errors");
     
     app.MapControllers();
     app.MapGet("/", () => "Hello World!");
